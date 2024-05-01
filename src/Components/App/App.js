31eb137acd5286381fc408011fe96a4de19e305c
bontapalle <rh364338@dal.ca>
2024-05-01 17:36:41 -0300

    change initial state of playlist and search

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -6,48 +6,10 @@ import SearchBar from '../SearchBar/SearchBar';
 import { Spotify } from '../../util/Spotify/Spotify';
 
 function App() {
-  const [searchResults, setSearchResults] = useState([
-    {
-      name: 'name 1',
-      artist: 'artist 1',
-      album: 'album 1',
-      id: 1,
-    },
-    {
-      name: 'name 2',
-      artist: 'artist 2',
-      album: 'album 2',
-      id: 2,
-    },
-    {
-      name: 'name 4',
-      artist: 'artist 4',
-      album: 'album 4',
-      id: 4,
-    },
-  ]);
+  const [searchResults, setSearchResults] = useState([]);
   const [playlistName, setPlaylistName] = useState('Playlist name');
 
-  const [playlistTracks, setPlaylistTracks] = useState([
-    {
-      name: 'name 1',
-      artist: 'artist 1',
-      album: 'album 1',
-      id: 1,
-    },
-    {
-      name: 'name 2',
-      artist: 'artist 2',
-      album: 'album 2',
-      id: 2,
-    },
-    {
-      name: 'name 3',
-      artist: 'artist 3',
-      album: 'album 3',
-      id: 3,
-    },
-  ]);
+  const [playlistTracks, setPlaylistTracks] = useState([]);
 
   function addTrack(track) {
     const existingTrack = playlistTracks.find((t) => t.id === track.id);
